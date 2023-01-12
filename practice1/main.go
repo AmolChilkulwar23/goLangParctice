28967fdbfc174d3c621c59d2324d46d6a0f32b18
amolchilkulwar <amol.chilkulwar@oantheon.io>
2023-01-12 17:57:24 +0530

    New Changes and Code

---
+++ practice1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	var out = x + y
+	return out
+}
+
+func main(){
+	num1 := 123
+	num2 := 890
+	result := add(num1,num2)
+	fmt.Println(result)
+}
