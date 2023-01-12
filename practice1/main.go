448ee38dc3a9e4401a379503865525eae519c4f6
amolchilkulwar <amol.chilkulwar@oantheon.io>
2023-01-12 18:07:56 +0530

    new changes

--- practice1/main.go
+++ practice1/main.go
@@ -7,9 +7,17 @@ func add(x int, y int) int {
 	return out
 }
 
+func calc(x, y int) (int, int, int) {
+	var out1 = x - y
+	out2 := x * y
+	out3 := x / y
+	return out1, out2, out3
+}
+
 func main(){
 	num1 := 123
 	num2 := 890
 	result := add(num1,num2)
-	fmt.Println(result)
+	result1, result2, result3 := calc(num1, num2)
+	fmt.Println(result,"\n",result1,"\n",result2,"\n",result3)
 }
